aa94e18cf93cfe0e984dc42570231df5e957175a
Nicholas Brereton <nicholasrbrereton@gmail.com>
2023-03-21 13:37:40 -0400

    mock challenge Complete

--- src/artist.py
+++ src/artist.py
@@ -1,2 +1,23 @@
 class Artist:
-    pass
+    def __init__(self, name):
+        self.name = name
+        self.records = []
+    
+    def get_name(self):
+        return self.name
+    
+    def get_records(self):
+        return self.records 
+    
+    def get_first_record(self):
+        min_record = self.records[0]
+        for i in self.records:
+            if i.year < min_record.year:
+                min_record = i 
+            return min_record
+
+    
+
+    
+
+     
--- src/record.py
+++ src/record.py
@@ -1,2 +1,37 @@
-class Record:
-    pass
+class Record():
+
+    def __init__(self, title, artist, year):
+        self.title = title
+        self.year = year
+        self.artist = artist
+        self.songs = []
+
+    def get_title(self):
+        return self.title.title()
+    
+    def get_artist(self):
+        return self.artist
+    
+    def get_year(self):
+        return self.year
+    
+    def get_songs(self):
+        return self.songs
+   
+    def total_runtime(self):
+       return sum(song.runtime for song in self.songs)
+    
+    def has_song(self, song):
+        if song in self.songs:
+            return True 
+        else: 
+            return False
+        
+    def get_longest_song(self):
+        longest = self.songs[0]
+        for i in self.songs:
+            if i.runtime > longest.runtime:
+                longest = i 
+            return longest
+        
+
